b09755fcd2ac67a4f05a9b61d4ef08ae46b8d740
ahamedhafee <ahamedhafeel29@gmail.com>
2025-11-01 17:36:26 +0530

    removeDuplicate.js

--- problems/removeDuplicate.js
+++ problems/removeDuplicate.js
@@ -1,3 +1,6 @@
+
+// remove duplicate letters from string
+
 function removeDuplicate(str){
     let word = '';
 
@@ -14,3 +17,15 @@ const inputWord = "Hello World";
 const finalWord = removeDuplicate(inputWord);
 
 console.log(finalWord);
+
+//remove duplicate value from Arrys
+
+function removeDuplicateNum(arr){
+    arr.sort();
+    return [...new Set(arr)];
+}
+
+
+const array = [1,2,2,5,9,4,5,9,6,6];
+console.log(removeDuplicateNum(array));
+
